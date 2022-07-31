3abaf8aa40af2a3a8b5a14d202a03d8292ad4ad3
Paul Razvan Berg <hello@paulrberg.com>
2022-07-31 12:25:58 +0300

    test: import and use Cheats from Forge Std
    build: update to forge-std v0.3

--- test/Foo.t.sol
+++ test/Foo.t.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.4;
 
+import { Cheats } from "forge-std/Cheats.sol";
 import { console } from "forge-std/console.sol";
 import { PRBTest } from "@prb/test/PRBTest.sol";
 
 /// @dev See the "Writing Tests" section in the Foundry Book if this is your first time with Forge.
 /// https://book.getfoundry.sh/forge/writing-tests
-contract ContractTest is PRBTest {
+contract ContractTest is PRBTest, Cheats {
     function setUp() public {
         // solhint-disable-previous-line no-empty-blocks
     }
