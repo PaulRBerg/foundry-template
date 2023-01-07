fb77ad354933bb1008247140e5d879e77741e6a5
Paul Razvan Berg <hello@paulrberg.com>
2023-01-07 11:50:02 +0200

    chore: reorder imports

--- test/Foo.t.sol
+++ test/Foo.t.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.4;
 
-import { console2 } from "forge-std/console2.sol";
 import { PRBTest } from "@prb/test/PRBTest.sol";
+import { console2 } from "forge-std/console2.sol";
 import { StdCheats } from "forge-std/StdCheats.sol";
 
 /// @dev See the "Writing Tests" section in the Foundry Book if this is your first time with Forge.
