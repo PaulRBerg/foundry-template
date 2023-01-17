0efda5eabbd82532d238f2eb7cc89f98c9917120
Paul Razvan Berg <hello@paulrberg.com>
2023-01-17 19:49:14 +0200

    test: add Ethereum Mainnet fork test
    feat: add "ALCHEMY_API_KEY" env var
    feat: add "ethereum"
    ci: delete now-stale "version" param in foundry-toolchain
    refactor: delete "ETH_RPC_URL" env var
    test: delete console log in "testFuzz_Example"
    test: use "assertGt" instead of "assertTrue" for "x > 0" comparison

--- test/Foo.t.sol
+++ test/Foo.t.sol
@@ -5,6 +5,10 @@ import { PRBTest } from "@prb/test/PRBTest.sol";
 import { console2 } from "forge-std/console2.sol";
 import { StdCheats } from "forge-std/StdCheats.sol";
 
+interface IERC20 {
+    function balanceOf(address account) external view returns (uint256);
+}
+
 /// @dev See the "Writing Tests" section in the Foundry Book if this is your first time with Forge.
 /// https://book.getfoundry.sh/forge/writing-tests
 contract FooTest is PRBTest, StdCheats {
@@ -19,10 +23,24 @@ contract FooTest is PRBTest, StdCheats {
         assertTrue(true);
     }
 
-    /// @dev Test that fuzzes an unsigned integer. Run Forge with `-vvvv` to see console logs.
+    /// @dev Test that fuzzes an unsigned integer.
     function testFuzz_Example(uint256 x) external {
         vm.assume(x != 0);
-        console2.log("x", x);
-        assertTrue(x > 0);
+        assertGt(x, 0);
+    }
+
+    /// @dev Test that runs against a fork of Ethereum Mainnet. You need to set `ALCHEMY_API_KEY` in your environment
+    /// for this test to run - you can get an API key for free at https://alchemy.com.
+    function testFork_Example() external {
+        string memory alchemyApiKey = vm.envOr("ALCHEMY_API_KEY", string(""));
+        if (bytes(alchemyApiKey).length == 0) {
+            return;
+        }
+        vm.createSelectFork({urlOrAlias: "ethereum", blockNumber: 16_428_000});
+        address usdc = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+        address holder = 0x7713974908Be4BEd47172370115e8b1219F4A5f0;
+        uint256 actualBalance = IERC20(usdc).balanceOf(holder);
+        uint256 expectedBalance = 196_307_713.810457e6;
+        assertEq(actualBalance, expectedBalance);
     }
 }
