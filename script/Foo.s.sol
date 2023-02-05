a3a2d3d951e505ae2121051ba72c2c3d365a8385
Paul Razvan Berg <hello@paulrberg.com>
2023-02-05 13:06:26 +0200

    refactor: change pragma to ">=0.8.18"

--- script/Foo.s.sol
+++ script/Foo.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.4;
+pragma solidity >=0.8.18;
 
 import { Script } from "forge-std/Script.sol";
 import { Foo } from "../src/Foo.sol";
--- src/Foo.sol
+++ src/Foo.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.4;
+pragma solidity >=0.8.18;
 
 contract Foo {
 // solhint-disable-previous-line no-empty-blocks
