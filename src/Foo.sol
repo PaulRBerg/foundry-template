0895f349322a419d90002116ff286dd37a731d88
Paul Razvan Berg <paul.razvan.berg@gmail.com>
2023-03-04 12:37:52 +0200

    build: bump "prb-test"
    style: fix formatting in "Foo.sol"

--- src/Foo.sol
+++ src/Foo.sol
@@ -2,11 +2,7 @@
 pragma solidity >=0.8.19;
 
 contract Foo {
-    function id(uint256 value)
-        external
-        pure
-        returns (uint256)
-    {
+    function id(uint256 value) external pure returns (uint256) {
         return value;
     }
 }
