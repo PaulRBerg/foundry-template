145cb6f7c963e7b7e0ea362723b83db51609dc62
r4bbit <445106+0x-r4bbit@users.noreply.github.com>
2023-08-08 13:35:14 +0200

    chore: first template adjustments
    This is an initial take on adjusting the foundry-template to the Vac's
    smart contract unit's needs. In a nutshell what this does is:
    
    1. Update README to not refer to upstream repo where not necessary
    2. Removes PRBergTest library from dependencies
    3. Removes `FUNDING.qml`
    4. Adjust CI actions
    
    There are more things to be done and decided on in follow-up commits.

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,12 @@
+## Description
+
+Describe the changes made in your pull request here.
+
+## Checklist
+
+Ensure you completed **all of the steps** below before submitting your pull request:
+
+- [ ] Added natspec comments?
+- [ ] Ran `forge snapshot`?
+- [ ] Ran `yarn lint`?
+- [ ] Ran `forge test`?
--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,10 +2,8 @@
 pragma solidity >=0.8.19 <=0.9.0;
 
 import { Foo } from "../src/Foo.sol";
-
 import { BaseScript } from "./Base.s.sol";
 
-/// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
 contract Deploy is BaseScript {
     function run() public broadcast returns (Foo foo) {
         foo = new Foo();
